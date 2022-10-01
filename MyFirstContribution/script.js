9137d93d8757bc4a7d4835642c51d114315e9861
Suhaas <suhaas@anattadesign.com>
2022-10-01 17:52:24 +0530

    added proper animation and contributon details

--- MyFirstContribution/script.js
+++ MyFirstContribution/script.js
@@ -96,6 +96,44 @@ function addElement(userData) {
     const currentDiv = document.getElementById("containerID1");
     currentDiv.appendChild(card);
 
+    //wobble animation for title start
+    var el = document.querySelector('#first');
+    el.addEventListener('mouseover', function(){
+            
+        if(!el.classList.contains('animating') && !el.classList.contains('mouseover')){
+                
+            el.classList.add('animating','mouseover');
+            var letters = el.innerText.split('');
+                    
+            setTimeout(function(){ el.classList.remove('animating'); }, (letters.length + 1) * 50);
+                    
+            var animationName = el.dataset.animation;
+            if(!animationName){ animationName = "jump"; }
+                
+            el.innerText = '';
+                
+            letters.forEach(function(letter){
+                if(letter == " "){
+                    letter = "&nbsp;";
+                }
+                el.innerHTML += '<span class="letter">'+letter+'</span>';
+            });
+                    
+            var letterElements = el.querySelectorAll('.letter');
+            letterElements.forEach(function(letter, i){
+                setTimeout(function(){
+                    letter.classList.add(animationName);
+                }, 50 * i);
+            });
+                    
+        }
+            
+    });
+        
+    el.addEventListener('mouseout', function(){				
+        el.classList.remove('mouseover');
+    });    
+    //wobble animation for title end
 }
 
 // Adding multiple elements
